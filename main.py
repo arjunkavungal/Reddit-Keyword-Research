2250d78a6167f67131166524b7635a41220c3bd6
Arjun Kavungal <47582621+yimen3@users.noreply.github.com>
2023-03-03 20:03:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from flask import Flask, jsonify
-import os
-
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+import praw
+import sys
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from model import *
+import pickle
+nltk.download('punkt')
+nltk.download('stopwords')
+plt.switch_backend('Agg') 
+name = ""
+@app.route("/")
+def home():
+    return render_template('index.html')
+@app.route('/process', methods=['POST'])
+def process():
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    name = request.form['name'] # get the value in the text box
 
+    if name:
+        df = get_hot_titles(name)
+        df = get_title_length(df)
+        df = get_word_count(df)
+        df = unweighted_word_count(df)
+        for i in df.columns[4:]:
+            df[i] = df[i] * df['ups']
+        df = df.append(df.sum().rename('Total'))      
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+        return jsonify({'name' : " ".join(df.iloc[-1][4:-1].astype(int).nlargest(25).keys().values.tolist()), 'val':' '.join(str(x) for x in df.iloc[-1][4:-1].astype(int).nlargest(25).values.tolist())})
